5ca0db85c38a82478f416ec7a2b3dafa3c105800
Vojtech Bocek <vbocek@gmail.com>
2019-01-07 23:52:45 +0100

    Version 2.0.0

--- src/RBControl_version.hpp
+++ src/RBControl_version.hpp
@@ -4,6 +4,6 @@
 #define RB3201_RBControl
 #define RBControl
 
-#define RB3201_MAJOR 1
-#define RB3201_MINOR 6
-#define RB3201_PATCH 1
+#define RB3201_MAJOR 2
+#define RB3201_MINOR 0
+#define RB3201_PATCH 0
