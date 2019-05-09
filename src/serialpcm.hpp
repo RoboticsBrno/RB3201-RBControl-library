cfeedc8b09a8ab8f90ac8c569c00f401c6c39a8e
Kuba Streit <kuba.streit@gmail.com>
2019-05-09 13:48:51 +0200

    Change PCM to PWM

--- src/serialpcm.hpp
+++ src/serialpcm.hpp
@@ -12,6 +12,8 @@
 // This file is part of RB3201-RBControl-library - https://github.com/RoboticsBrno/RB3201-RBControl-library
 // Author: Jakub Streit
 
+//#include"hexdump.h"
+
 /// @private
 class SerialPCM {
 public:
@@ -24,7 +26,7 @@ public:
               const int test_pin = -1,
               i2s_dev_t& i2s = I2S1)
         : c_channels (channels),
-          c_bytes(((data_pins.size() + 1)>>3) + 1), // The first +1 is for latch pin
+          c_bytes(((data_pins.size() + (test_pin == -1 ? 0 : 1))>>3) + 1),
           m_i2s (&i2s),
           m_buffer_descriptors {nullptr},
           m_buffer {nullptr},
@@ -34,21 +36,18 @@ public:
         const int buffer_size = c_channels * c_bytes;
         for (int buffer = 0; buffer != sc_buffers; ++buffer) {
             m_buffer_descriptors[buffer] = static_cast<i2s_parallel_buffer_desc_t*>(heap_caps_malloc(sc_resolution * sizeof(i2s_parallel_buffer_desc_t), MALLOC_CAP_32BIT));
-            for (int bit = 0; bit != sc_bit_depth; ++bit) {
+            for (int i = 0; i != sc_resolution; ++i) {
                 uint8_t* p_buffer = static_cast<uint8_t*>(heap_caps_malloc(buffer_size, MALLOC_CAP_DMA));
-                for (int i = 0; i != (1<<bit); ++i) {
-                    int j = (1<<(sc_bit_depth-bit-1));
-                    j += (j<<1) * i - 1;
-                    m_buffer_descriptors[buffer][j].memory = p_buffer;
-                    m_buffer_descriptors[buffer][j].size = buffer_size;
-                }
+                m_buffer_descriptors[buffer][i].memory = p_buffer;
+                m_buffer_descriptors[buffer][i].size = buffer_size;
                 memset(p_buffer, 0, buffer_size);
-                p_buffer[0] = (1<<(data_pins.size()&7));
-                m_buffer[buffer][bit] = p_buffer;
+                p_buffer[c_bytes - 1] = (1<<(data_pins.size()&7)); // latch pin
+                m_buffer[buffer][i] = p_buffer;
             }
             m_buffer_descriptors[buffer][sc_resolution-1].memory = nullptr;
-            for (int i = 0; i != buffer_size; ++i)
-                m_buffer[buffer][0][i] |= 1<<(data_pins.size() + 1);
+            if (test_pin != -1)
+                for (int i = 0; i != buffer_size; ++i)
+                    m_buffer[buffer][0][i] |= 1<<(data_pins.size() + 1);
         }
 
         i2s_parallel_config_t cfg;
@@ -71,7 +70,7 @@ public:
         cfg.inv_wclk = false;
         cfg.gpio_bclk = clock_pin;
         cfg.inv_bclk = false;
-        cfg.clkspeed = 255;
+        cfg.clkspeed = 1;
         cfg.bufa = m_buffer_descriptors[0];
         cfg.bufb = m_buffer_descriptors[1];
 
@@ -95,34 +94,30 @@ public:
 
     void update() {
         m_active_buffer ^= 1;
-        int i = m_pwm.size();
-        for (int channel = 0; channel != m_pwm.size(); ++channel) {
-            uint8_t mask = 1<<(channel / c_channels);
-            const int pos = ((channel&7) | ((channel&~7)*c_bytes)) % c_channels;
-            --i;
-            for (int bit = 0; bit != sc_bit_depth; ++bit) {
-                uint8_t& value = m_buffer[m_active_buffer][bit][pos];
-                if (m_pwm[channel] & (1<<bit))
-                    value |= mask;
+        for (int sample = 0; sample != sc_resolution; ++sample) {
+            for (int channel = 0; channel != m_pwm.size(); ++channel) {
+                uint8_t& value = m_buffer[m_active_buffer][sample][(channel % c_channels) * c_bytes + ((channel / c_channels)>>3)];
+                if (sample < m_pwm[channel])
+                    value |=  (1<<((channel / c_channels) & 7));
                 else
-                    value &= ~mask;
+                    value &= ~(1<<((channel / c_channels) & 7));
             }
         }
         i2s_parallel_flip_to_buffer(m_i2s, m_active_buffer);
     }
 
-    int resolution() const { return sc_resolution - 1; }
+    static int resolution() { return sc_resolution; }
 
 private:
 
     static const int sc_buffers = 2;
     static const int sc_bit_depth = 8;
-    static const int sc_resolution = 1<<sc_bit_depth;
+    static const int sc_resolution = (1<<sc_bit_depth) - 1;
     const int c_channels;
     const int c_bytes;
     i2s_dev_t* m_i2s;
     i2s_parallel_buffer_desc_t* m_buffer_descriptors[sc_buffers];
-    uint8_t* m_buffer[sc_buffers][sc_bit_depth];
+    uint8_t* m_buffer[sc_buffers][sc_resolution];
     int m_active_buffer;
     std::vector<value_type> m_pwm;
 };
