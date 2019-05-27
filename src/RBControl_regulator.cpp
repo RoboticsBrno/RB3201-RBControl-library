4163bf60f67f7fc0d6238a3a87742887ba02f407
Kuba Streit <kuba.streit@gmail.com>
2019-05-28 00:07:39 +0200

    Regulator process frame, without actually calling it

--- src/RBControl_regulator.cpp
+++ src/RBControl_regulator.cpp
@@ -1,11 +1,73 @@
 #include "RBControl_regulator.hpp"
 
+#include <freertos/FreeRTOS.h>
+
+#include <esp_log.h>
+
+#define TAG "RbRegulator"
+
 namespace rb {
 
-Regulator::Regulator(Motor& motor)
-    : m_motor(motor)
+Regulator::Regulator()
+    : m_reader(nullptr),
+      m_writer(nullptr)
 {}
 
-void Regulator::install() {}
+Regulator::~Regulator() {
+    std::lock_guard<std::mutex> guard(s_mutex);
+    for (auto i = s_instances.begin(); i != s_instances.end(); ++i) {
+        if (*i == this) {
+            s_instances.erase(i);
+            return;
+        }
+    }
+}
+
+void Regulator::install(InputReader inr, OutputWriter outw) {
+    if (!inr || !outw) {
+        ESP_LOGE(TAG, "Both InputReader and OutputWriter has to be valid functions!");
+        abort();
+    }
+    std::lock_guard<std::mutex> guard(s_mutex);
+    if (s_instances.empty()) {
+        s_instances.push_back(this);
+    } else {
+        for (auto reg: s_instances)
+            if (reg == this) {
+                ESP_LOGW(TAG, "Regulator::install should be called only once per Regulator.");
+                return;
+            }
+        s_instances.push_back(this);
+    }
+    m_reader = inr;
+    m_writer = outw;
+}
+
+std::mutex Regulator::s_mutex;
+std::vector<Regulator*> Regulator::s_instances;
+std::vector<std::function<void()> > Regulator::s_preprocessors;
+std::vector<std::function<void()> > Regulator::s_postprocessors;
+
+void Regulator::add_preprocessor(std::function<void()> fcn) {
+    std::lock_guard<std::mutex> guard(s_mutex);
+    s_preprocessors.push_back(fcn);
+}
+
+void Regulator::add_postprocessor(std::function<void()> fcn) {
+    std::lock_guard<std::mutex> guard(s_mutex);
+    s_postprocessors.push_back(fcn);
+}
+
+void Regulator::process_trampoline() {
+    std::lock_guard<std::mutex> guard(s_mutex);
+    for (auto& fcn: s_preprocessors)
+        if (fcn)
+            fcn();
+    for (auto reg: s_instances)
+        reg->process();
+    for (auto& fcn: s_postprocessors)
+        if (fcn)
+            fcn();
+}
 
 } // namespace rb
--- src/RBControl_regulator.hpp
+++ src/RBControl_regulator.hpp
@@ -1,18 +1,42 @@
 #pragma once
 
+#include <vector>
+#include <mutex>
+#include <functional>
+
 namespace rb {
 
 class Motor;
 
 class Regulator {
 public:
-    Regulator(Motor& motor);
+    typedef float Num;
+    typedef std::function<Num()> InputReader;
+    typedef std::function<void(Num)> OutputWriter;
+
+    Regulator(Regulator const&) = delete;
+    void operator=(Regulator const&) = delete;
+
+    Regulator();
+
+    ~Regulator();
     
-    void install();
+    void install(InputReader inr, OutputWriter outw);
 
     void process();
+
+    static void add_preprocessor(std::function<void()> fcn);
+    static void add_postprocessor(std::function<void()> fcn);
 private:
-    Motor& m_motor;
+    InputReader m_reader;
+    OutputWriter m_writer;
+
+    static void process_trampoline();
+
+    static std::mutex s_mutex;
+    static std::vector<Regulator*> s_instances;
+    static std::vector<std::function<void()> > s_preprocessors;
+    static std::vector<std::function<void()> > s_postprocessors;
 };
 
 } // namespace rb
