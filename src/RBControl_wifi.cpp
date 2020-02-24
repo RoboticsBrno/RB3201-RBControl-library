c09a135ed8b41e7fff9c8c004a36baffa284238b
Vojtech Bocek <vbocek@gmail.com>
2020-02-24 22:54:10 +0100

    Add getIp() method to WiFi

--- src/RBControl_wifi.cpp
+++ src/RBControl_wifi.cpp
@@ -2,7 +2,6 @@
 #include "freertos/event_groups.h"
 #include "esp_wifi.h"
 #include "esp_log.h"
-#include "esp_event_loop.h"
 #include "nvs_flash.h"
 #include <string.h>
 
@@ -10,65 +9,14 @@
 
 #define TAG "RbWifi"
 
-static esp_err_t event_handler(void *ctx, system_event_t *event)
-{
-    switch (event->event_id) {
-        case SYSTEM_EVENT_STA_START:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_START");
-            tcpip_adapter_dhcpc_start(TCPIP_ADAPTER_IF_STA);
-            ESP_ERROR_CHECK(esp_wifi_connect());
-            break;
-        case SYSTEM_EVENT_STA_STOP:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_STOP");
-            tcpip_adapter_dhcpc_stop(TCPIP_ADAPTER_IF_STA);
-            break;
-        case SYSTEM_EVENT_STA_GOT_IP:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
-            ESP_LOGI(TAG, "Got IP: %s\n",
-                     ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
-            break;
-        case SYSTEM_EVENT_STA_DISCONNECTED:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_DISCONNECTED");
-            ESP_ERROR_CHECK(esp_wifi_connect());
-            break;
-        case SYSTEM_EVENT_AP_START: {
-            ESP_LOGI(TAG, "SYSTEM_EVENT_AP_START");
-            tcpip_adapter_ip_info_t info;
-            IP4_ADDR(&info.ip, 192, 168, 0, 1);
-            IP4_ADDR(&info.gw, 192, 168, 0, 1);
-            IP4_ADDR(&info.netmask, 255, 255, 255, 0);
-            tcpip_adapter_dhcps_stop(TCPIP_ADAPTER_IF_AP);
-            ESP_ERROR_CHECK(tcpip_adapter_set_ip_info(TCPIP_ADAPTER_IF_AP, &info));
+namespace rb {
 
-            dhcps_lease_t lease;
-            lease.enable = true;
-            IP4_ADDR(&lease.start_ip, 192, 168, 0, 10);
-            IP4_ADDR(&lease.end_ip, 192, 168, 0, 250);
-
-            tcpip_adapter_dhcps_option(
-                (tcpip_adapter_option_mode_t)TCPIP_ADAPTER_OP_SET,
-                (tcpip_adapter_option_id_t)TCPIP_ADAPTER_REQUESTED_IP_ADDRESS,
-                (void*)&lease, sizeof(dhcps_lease_t)
-            );
-
-            ESP_ERROR_CHECK(tcpip_adapter_dhcps_start(TCPIP_ADAPTER_IF_AP));
-            break;
-        }
-        case SYSTEM_EVENT_AP_STOP:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_AP_STOP");
-            tcpip_adapter_dhcps_stop(TCPIP_ADAPTER_IF_AP);
-            break;
-        case SYSTEM_EVENT_AP_STACONNECTED:
-            ESP_LOGI(TAG, "SYSTEM_EVENT_AP_STACONNECTED");
-            break;
-        default:
-            ESP_LOGI(TAG, "Unhandled event %d", (int)event->event_id);
-            break;
-    }
-    return ESP_OK;
-}
+static const ip4_addr_t EMPTY_IP = {
+    .addr = 0,
+};
 
 class WiFiInitializer {
+    friend class WiFi;
 public:
     WiFiInitializer() {
         esp_err_t ret = nvs_flash_init();
@@ -80,7 +28,7 @@ public:
 
         tcpip_adapter_init();
 
-        ESP_ERROR_CHECK(esp_event_loop_init(&event_handler, NULL));
+        ESP_ERROR_CHECK(esp_event_loop_init(&WiFi::eventHandler, NULL));
 
         wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
         ESP_ERROR_CHECK(esp_wifi_init(&cfg));
@@ -93,7 +41,11 @@ public:
     }
 };
 
-namespace rb {
+std::atomic<ip4_addr_t> WiFi::m_ip;
+
+void WiFi::init() {
+    static WiFiInitializer init;
+}
 
 void WiFi::connect(const char *ssid, const char *pass) {
     init();
@@ -137,8 +89,67 @@ void WiFi::startAp(const char *ssid, const char *pass, uint8_t channel) {
     esp_wifi_set_bandwidth(WIFI_IF_AP, WIFI_BW_HT20);
 }
 
-void WiFi::init() {
-    static WiFiInitializer init;
+esp_err_t WiFi::eventHandler(void *ctx, system_event_t *event)
+{
+    switch (event->event_id) {
+        case SYSTEM_EVENT_STA_START:
+            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_START");
+            tcpip_adapter_dhcpc_start(TCPIP_ADAPTER_IF_STA);
+            ESP_ERROR_CHECK(esp_wifi_connect());
+            break;
+        case SYSTEM_EVENT_STA_STOP:
+            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_STOP");
+            tcpip_adapter_dhcpc_stop(TCPIP_ADAPTER_IF_STA);
+            break;
+        case SYSTEM_EVENT_STA_GOT_IP:
+            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
+            ESP_LOGI(TAG, "Got IP: %s\n",
+                     ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
+            m_ip.store(event->event_info.got_ip.ip_info.ip);
+            break;
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+            m_ip.store(EMPTY_IP);
+            ESP_LOGI(TAG, "SYSTEM_EVENT_STA_DISCONNECTED");
+            esp_wifi_connect();
+            break;
+        case SYSTEM_EVENT_AP_START: {
+            ESP_LOGI(TAG, "SYSTEM_EVENT_AP_START");
+            tcpip_adapter_ip_info_t info;
+            IP4_ADDR(&info.ip, 192, 168, 0, 1);
+            IP4_ADDR(&info.gw, 192, 168, 0, 1);
+            IP4_ADDR(&info.netmask, 255, 255, 255, 0);
+            tcpip_adapter_dhcps_stop(TCPIP_ADAPTER_IF_AP);
+            ESP_ERROR_CHECK(tcpip_adapter_set_ip_info(TCPIP_ADAPTER_IF_AP, &info));
+
+            dhcps_lease_t lease;
+            lease.enable = true;
+            IP4_ADDR(&lease.start_ip, 192, 168, 0, 10);
+            IP4_ADDR(&lease.end_ip, 192, 168, 0, 250);
+
+            tcpip_adapter_dhcps_option(
+                (tcpip_adapter_option_mode_t)TCPIP_ADAPTER_OP_SET,
+                (tcpip_adapter_option_id_t)TCPIP_ADAPTER_REQUESTED_IP_ADDRESS,
+                (void*)&lease, sizeof(dhcps_lease_t)
+            );
+
+            ESP_ERROR_CHECK(tcpip_adapter_dhcps_start(TCPIP_ADAPTER_IF_AP));
+
+            m_ip.store(info.ip);
+            break;
+        }
+        case SYSTEM_EVENT_AP_STOP:
+            ESP_LOGI(TAG, "SYSTEM_EVENT_AP_STOP");
+            tcpip_adapter_dhcps_stop(TCPIP_ADAPTER_IF_AP);
+            m_ip.store(EMPTY_IP);
+            break;
+        case SYSTEM_EVENT_AP_STACONNECTED:
+            ESP_LOGI(TAG, "SYSTEM_EVENT_AP_STACONNECTED");
+            break;
+        default:
+            ESP_LOGI(TAG, "Unhandled event %d", (int)event->event_id);
+            break;
+    }
+    return ESP_OK;
 }
 
 }; // namespace rb
